5a7a81edcc47a68a96aaa968ca90f531f51e5871
Abdulrahman Ismail <abdulrahman.ismail.mohammed@gmail.com>
2022-12-26 02:48:35 +0200

    create single event

--- controller/event.controller.js
+++ controller/event.controller.js
@@ -1,18 +1,37 @@
 const Event = require("../model/event.model");
 
 const getAllEvent = async (req, res) => {
-    const events = await Event.find({});
-    let chunk = []
-    let chunkSize = 3
-    for (let i = 0; i < events.length; i += chunkSize) {
-        chunk.push(events.slice(i, chunkSize + i))
+    try {
+        const events = await Event.find({});
+        let chunk = []
+        let chunkSize = 3
+        let eventLength = events.length;
+        for (let i = 0; i < eventLength; i += chunkSize) {
+            chunk.push(events.slice(i, chunkSize + i))
+        }
+        res.render('event/index', {
+            chunk: chunk
+        })
+    } catch (error) {
+        res.status(500).json(error)
     }
-    //res.json(chunk)
-    res.render('event/index', {
-        chunk: chunk
-    })
+};
+
+const getSingleEvent = async (req, res) => {
+    try {
+        const { id: eventId } = req.params;
+        const event = await Event.findOne({ _id: eventId });
+        if (!event) return res.status(404).send("Not Found");
+        res.render('event/show', {
+            event: event
+        });
+    } catch (error) {
+        res.status(500).json(error)
+    }
+
 }
 
 module.exports = {
-    getAllEvent
+    getAllEvent,
+    getSingleEvent
 }
--- routes/event.routes.js
+++ routes/event.routes.js
@@ -1,13 +1,10 @@
-const { getAllEvent } = require('../controller/event.controller');
+const { getAllEvent, getSingleEvent } = require('../controller/event.controller');
 
 const router = require('express').Router();
 
 router.get('/', getAllEvent);
 
-router.get('/:id', (req, res) => {
-    res.render("event/show")
-});
-
+router.get('/:id', getSingleEvent);
 
 
 module.exports = router
